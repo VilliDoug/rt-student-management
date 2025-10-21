397d412b9c941389c91a5be30afad892b122dca1
VilliDoug <villidoug@gmail.com>
2025-10-21 23:08:26 +0900

    WIP: Application.java changes before updating from remote

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -2,12 +2,10 @@ package raisetech.student.management;
 
 import java.util.HashMap;
 import java.util.Map;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -51,9 +49,17 @@ public class Application {
   public void updateStudentName(String name) {
     this.name = name;
   }
-  //GET POST
-  //GETは取得する、リクエストの結果を受け取る
-  //POSTは情報を与える、渡す
+
+  @PostMapping("/mapInfo")
+  public void addMapInfo (String name, String age) {
+    studentMap.put(name, age);
+    this.name = name;
+    this.age = age;
+  }
 }
+
+//GET POST
+//GETは取得する、リクエストの結果を受け取る
+//POSTは情報を与える、渡す
 
 
