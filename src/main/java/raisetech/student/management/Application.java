60c3d065505215bb168d2ecf0ef7384a395081fd
VilliDoug <villidoug@gmail.com>
2025-10-24 00:29:02 +0900

    講義20の内容と課題の準備完了。

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -2,59 +2,66 @@ package raisetech.student.management;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-  private String name = "Gui Migotto";
-  private String age = "36";
+  @Autowired
+  private StudentRepository repository;
 
   private Map<String, String> studentMap = new HashMap<>();
-
 
   //try to add and update students inside the map
 
 	public static void main(String[] args) {
     // localhost:8080
     SpringApplication.run(Application.class, args);
-
 	}
-   //マップの管理について少しAIを使って書いてみましたが、先のことに行ってしまった感じがします。
-   //JSONの話やコマンドラインについてたくさんの情報だしてきましたので、混乱中です。一旦ここで今回おわりにします。
+
+  //課題
+  //セレクトのところを1件の取得から、WHERE文以降消して、一覧全件取得する。
+  //リストのstudentをとって、全体を出力する。
+
   @GetMapping("/mapInfo")
   public Map<String, String> getMapInfo() {
     return studentMap;
   }
 
-  //{"timestamp":"2025-10-21T00:03:16.351+00:00","status":400,"error":"Bad Request","path":"/mapInfo"}
-
-  @GetMapping("/studentInfo")
-  public String getStudentInfo() {
-    return name + " " + age + "歳";
+  @GetMapping("/student")
+  public String getStudent(@RequestParam("name") String name) {
+    Student student = repository.searchByName(name);
+    return student.getName() + " " + student.getAge() + "歳";
   }
 
-  @PostMapping("/studentInfo")
-  public void setStudentInfo(String name, String age) {
-    this.name = name;
-    this.age = age;
+  @PostMapping("/student")
+  public void registerStudent(String name, int age) {
+    repository.registerStudent(name, age);
   }
 
-  @PostMapping("/studentName")
-  public void updateStudentName(String name) {
-    this.name = name;
+  @PatchMapping("/student")
+  public void updateStudentName(String name, int age) {
+    repository.updateStudent(name, age);
+  }
+
+  @DeleteMapping("/student")
+  public void  deleteStudent(String name){
+    repository.deleteStudent(name);
+
   }
 
   @PostMapping("/mapInfo")
   public void addMapInfo (String name, String age) {
     studentMap.put(name, age);
-    this.name = name;
-    this.age = age;
   }
 }
 
