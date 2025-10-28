696567b550a39bd4b984e1a4c546f4d287a88c97
VilliDoug <villidoug@gmail.com>
2025-10-28 22:42:32 +0900

    「講義24」の課題を完了。提出するためにPRを作る。

---
+++ src/main/java/raisetech/student/management/service/StudentService.java
@@ -0,0 +1,54 @@
+package raisetech.student.management.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import raisetech.student.management.data.Student;
+import raisetech.student.management.data.StudentCourse;
+import raisetech.student.management.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+  private StudentRepository repository;
+
+  @Autowired
+  public StudentService(StudentRepository repository) {
+    this.repository = repository;
+  }
+
+  @GetMapping("/studentList")
+  public List<Student> searchStudentList() {
+    // 検索処理
+    //　絞り込みをする。年齢が25yo+の人のみを抽出する。
+    //　抽出したリストをコントローラーに渡す。
+    return repository.searchStudents();
+  }
+
+  @GetMapping("/filterList")
+  public List<Student> filterStudentList() {
+    List<Student> filteredStudentList = repository.searchStudents().stream()
+        .filter(n -> n.getAge() >= 25)
+        .collect(Collectors.toList());
+    return filteredStudentList;
+  }
+
+  @GetMapping("/courseFilterList")
+  public List<StudentCourse> filterCourseList() {
+    List<StudentCourse> filteredCourseList = repository.searchCourses().stream()
+      .filter(n -> n.getCourseName().equals("Java Course"))
+      .collect(Collectors.toList());
+    return filteredCourseList;
+  }
+
+  @GetMapping("/courseList")
+  public List<StudentCourse> searchStudentCourse() {
+    //　抽出絞り込み検索で「何かコース」のコース情報のみを抽出する。
+    //　抽出したリストをコントローラーに渡す。
+    return repository.searchCourses();
+  }
+
+
+}
